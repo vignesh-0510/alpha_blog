5f669be6f017ef117eeeabf636d1cc0cb5e2def5
Vignesh-0510 <vigneshkumar.0510@gmail.com>
2020-05-30 17:56:08 +0000

    Add users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -27,10 +36,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
-  end
 
   private
 
