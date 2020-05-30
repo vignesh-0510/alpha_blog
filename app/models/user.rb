cfe33a779233a8ac4dc7f411fe2fa871ba0a4ccf
Vignesh-0510 <vigneshkumar.0510@gmail.com>
2020-05-30 14:52:35 +0000

    Add password field to User Schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :articles
 
+  before_save {self.email = email.downcase}
   validates :username, presence: true,
                        uniqueness: {case_sensitive: false},
                        length: {in: 3..25}
@@ -12,4 +13,6 @@ class User < ApplicationRecord
                     uniqueness: {case_sensitive: false},
                     length: {maximum: 105},
                     format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
