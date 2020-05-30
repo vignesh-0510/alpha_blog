6e5e4028aed42deea5f3cbf14af37bb056e02dde
Vignesh-0510 <vigneshkumar.0510@gmail.com>
2020-05-30 16:39:12 +0000

    Add user edit page and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to articles_path, notice: "Account was updated Successfully!"
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
